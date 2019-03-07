998c0ec16c0cd13289f80b446609662779419cf2
Joel Bartlett <joel@murbar.com>
2019-03-07 10:17:43 -0500

    Create AddTodo

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useContext } from 'react';
+import TodosContext from '../context';
+
+const AddTodo = () => {
+  const [todoText, setTodoText] = useState('');
+  const {
+    state: { currentlyEditing },
+    dispatch
+  } = useContext(TodosContext);
+
+  useEffect(() => {
+    if (currentlyEditing.text) setTodoText(currentlyEditing.text);
+  }, [currentlyEditing]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (currentlyEditing.text) {
+      dispatch({ type: 'UPDATE_TODO', text: todoText });
+    } else {
+      dispatch({ type: 'ADD_TODO', text: todoText });
+    }
+    setTodoText('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" onChange={e => setTodoText(e.target.value)} value={todoText} />
+      <button>
+        <img src="https://icon.now.sh/add/eeeeee" alt="Edit" />
+      </button>
+    </form>
+  );
+};
+
+export default AddTodo;
