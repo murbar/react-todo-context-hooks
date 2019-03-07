0d1dacad851cf0449c024fb5a1470efa567d1642
Joel Bartlett <joel@murbar.com>
2019-03-07 00:13:40 -0500

    Add buttons

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,6 +9,12 @@ const TodoList = () => {
         {state.todos.map(todo => (
           <li key={todo.id}>
             <span>{todo.text}</span>
+            <button>
+              <img src="https://icon.now.sh/edit/eeeeee" alt="Edit" />
+            </button>
+            <button>
+              <img src="https://icon.now.sh/delete/eeeeee" alt="Delete" />
+            </button>
           </li>
         ))}
       </ul>
