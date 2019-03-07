70183c04365df300b13673481783d59742a37a74
Joel Bartlett <joel@murbar.com>
2019-03-07 00:39:40 -0500

    Add remove handler

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,16 +3,22 @@ import TodosContext from '../context';
 
 const TodoList = () => {
   const { state, dispatch } = useContext(TodosContext);
+
   return (
     <div>
       <ul>
         {state.todos.map(todo => (
           <li key={todo.id}>
-            <span>{todo.text}</span>
+            <span
+              data-complete={todo.complete}
+              onClick={() => dispatch({ type: 'TOGGLE_TODO', id: todo.id })}
+            >
+              {todo.text}
+            </span>
             <button>
               <img src="https://icon.now.sh/edit/eeeeee" alt="Edit" />
             </button>
-            <button>
+            <button onClick={() => dispatch({ type: 'REMOVE_TODO', id: todo.id })}>
               <img src="https://icon.now.sh/delete/eeeeee" alt="Delete" />
             </button>
           </li>
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,18 @@
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map(task => {
+          if (task.id === action.id) return { ...task, complete: !task.complete };
+          return task;
+        })
+      };
+    case 'REMOVE_TODO':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => todo.id !== action.id)
+      };
     default:
       return state;
   }
